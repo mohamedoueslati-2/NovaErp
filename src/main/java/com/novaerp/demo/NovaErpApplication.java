70a2a999bffa6071db3f9c998f231c83b7280750
Oueslati Mohamed <mohamedoueslati080@gmail.com>
2025-10-07 22:31:18 +0100

    Normalisation des fins de ligne

--- src/main/java/com/novaerp/demo/NovaErpApplication.java
+++ src/main/java/com/novaerp/demo/NovaErpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NovaErpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NovaErpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NovaErpApplication.class, args);
+    }
 
 }
--- src/test/java/com/novaerp/demo/NovaErpApplicationTests.java
+++ src/test/java/com/novaerp/demo/NovaErpApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NovaErpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
